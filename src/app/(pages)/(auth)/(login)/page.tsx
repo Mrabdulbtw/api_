bf8b2a2b2fc0a8e4a85b301d3190e832d858e769
Abdul <106859834+Mrabdulbtw@users.noreply.github.com>
2025-02-20 12:49:29 +0530

    Update page.tsx

--- src/app/(pages)/(auth)/(login)/page.tsx
+++ src/app/(pages)/(auth)/(login)/page.tsx
@@ -44,7 +44,7 @@ export default function Page() {
     const isValid: boolean = validation();
     if (isValid) {
      
-      const res = await fetch('http://localhost:5000/api/login', {
+      const res = await fetch('/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
